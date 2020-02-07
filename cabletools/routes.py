36b72d6461f78acde7d6921e7d0da0a3f53bfb25
Michael-A-Sherman <masherman3@liberty.edu>
2020-02-06 22:38:27 -0500

    removed /<node> route temporarily

--- cabletools/routes.py
+++ cabletools/routes.py
@@ -44,26 +44,26 @@ def hello():
     return render_template("calculator.html", form=form)
 
 
-@app.route("/<node>", methods=["POST", "GET"])
-def direct_link(node):
-    form = PathTrakForm(request.form)
-    info = search_pathtrak_api(node.upper().rstrip())
-    link = search(info)
-    if len(info) == 1:
-        return redirect(link, code=302)
-    elif len(info) == 0:
-        flash("Node Not Found, Try Again")
-    elif request.method == "POST":
-        if form.validate():
-            if len(info) == 1:
-                return redirect(link, code=302)
-            elif len(info) == 0:
-                flash("Node Not Found, Try Again")
-            else:
-                return render_template("results.html", form=form, link=link)
-    else:
-        return render_template("results.html", form=form, link=link)
-    return render_template("index.html", form=form)
+# @app.route("/<node>", methods=["POST", "GET"])
+# def direct_link(node):
+#     form = PathTrakForm(request.form)
+#     info = search_pathtrak_api(node.upper().rstrip())
+#     link = search(info)
+#     if len(info) == 1:
+#         return redirect(link, code=302)
+#     elif len(info) == 0:
+#         flash("Node Not Found, Try Again")
+#     elif request.method == "POST":
+#         if form.validate():
+#             if len(info) == 1:
+#                 return redirect(link, code=302)
+#             elif len(info) == 0:
+#                 flash("Node Not Found, Try Again")
+#             else:
+#                 return render_template("results.html", form=form, link=link)
+#     else:
+#         return render_template("results.html", form=form, link=link)
+#     return render_template("index.html", form=form)
 
 
 @app.route("/vpn", methods=["POST", "GET"])
